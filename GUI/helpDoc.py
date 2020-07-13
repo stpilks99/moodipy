9fa975ffa020b7ca02852991f89eda3b6120e37e
naomi <torrecardenasn@wit.edu>
2020-07-13 19:44:19 -0400

    Updated helpDoc.py

--- GUI/helpDoc.py
+++ GUI/helpDoc.py
@@ -1,6 +1,6 @@
 import sys
-import spotipy
-import spotipy.util as util
+#import spotipy
+#import spotipy.util as util
 import tkinter as tk 
 from tkinter import ttk
 from tkinter import *
@@ -13,7 +13,7 @@ helpDoc.geometry("900x680")
 
 #creates done button that brings to playlist window
 Home = Button(helpDoc, text = "Home", bg ="green", bd = 6, relief = "raised", width = 20, height = 6)
-Home.place(x = 710,y = 530)
+Home.place(x = 710,y = 550)
 
 #creates label with message 
 lm = tk.Label(helpDoc, 
@@ -24,7 +24,7 @@ lm = tk.Label(helpDoc,
     relief = "raised",
     font = "Helvetica 30 bold italic")
 
-lm.place(x = 150, y = 50)
+lm.place(x = 150, y = 35)
 
 # Q1
 q1 = tk.Label(helpDoc, 
@@ -34,7 +34,7 @@ q1 = tk.Label(helpDoc,
     bd = 6, 
     relief = "sunken",
     font = "Helvetica 18 bold italic")
-q1.place(x = 10, y = 120)
+q1.place(x = 40, y = 120)
 
 a1 = tk.Label(helpDoc,
     text = "Moodipy interfaces with the Spotify API to retrieve data on songs and playlists.\nIt organizes and sorts data to make playlist based on certain genres or moods.",
@@ -43,7 +43,7 @@ a1 = tk.Label(helpDoc,
     bd = 6, 
     relief = "sunken",
     font = "Helvetica 12 bold italic")
-a1.place(x = 30, y = 170)
+a1.place(x = 60, y = 170)
 
 q2 = tk.Label(helpDoc,
     text = "How many songs can I add to a playlist?",
@@ -52,7 +52,7 @@ q2 = tk.Label(helpDoc,
     bd = 6, 
     relief = "sunken",
     font = "Helvetica 18 bold italic")
-q2.place(x = 10, y = 230)
+q2.place(x = 40, y = 230)
 
 a2 = tk.Label(helpDoc,
     text = "Using Moodipy, each playlist has a max of 30 songs. Moodipy only adds songs \nit thinks you'll really like (based on moods, ranking, time periods and more) so \nyou'll never find yourself skipping through a bunch of songs you hate.",
@@ -61,9 +61,12 @@ a2 = tk.Label(helpDoc,
     bd = 6, 
     relief = "sunken",
     font = "Helvetica 12 bold italic")
-a2.place(x = 30, y = 280)
+a2.place(x = 60, y = 280)
 
 #another q: what does ranking a song do?
+
+myScrollbar = Scrollbar(helpDoc)
+myScrollbar.pack(side = RIGHT, fill = Y)
 
 
 helpDoc.mainloop()
