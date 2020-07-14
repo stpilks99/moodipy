8d95691a509b8efd251e340ab8ec71c536c3f613
bbass101 <58187347+bbass101@users.noreply.github.com>
2020-07-14 16:57:15 -0400

    Python SQL Functions
    Create Master Table
    Create Playlist table based on dummy information
    Save Database

---
+++ SQL Functions/main.py
@@ -0,0 +1,56 @@
+# Moodipy SQL Team SQL Database creation, and functions
+# remove existing database file if it exists.
+import os
+
+if os.path.exists("moodipy.db"):
+    os.remove("moodipy.db")
+    print("resetting db")
+else:
+    print("DB File does not exist")
+
+import array as arr  # havent used this yet
+
+import sqlite3
+
+database = sqlite3.connect("moodipy.db")
+cursor = database.cursor()
+
+setup_master = """CREATE TABLE "playlistmaster" (
+	"playlisturi"	CHAR(39) NOT NULL UNIQUE,
+	"username"	TEXT,
+	"playlistmood"	TEXT,
+	"playlistperiod"	TEXT,
+	"preferredartist"	TEXT,
+	"preferredgenre"	TEXT,
+	"explicit"	BOOL,
+	PRIMARY KEY("playlisturi")
+);"""
+# playlisturi is 39 characters long
+# songuri's are 36 characters long
+cursor.execute(setup_master)
+#example data
+playlisturi = "hello"
+playlistmood = "Happy"
+playlistperiod = "2000"
+preferredartist = "hamilton"
+preferredgenre = "rap"
+explicit = "true"
+#replace this data
+
+#create entry in playlist master table
+sqlcommand = "INSERT INTO playlistmaster (playlisturi, playlistmood,playlistperiod, preferredartist, preferredgenre, explicit) " +\
+          "VALUES('"+playlisturi+"','"+playlistmood+"','"+playlistperiod+"','"+preferredartist+"','"+preferredgenre+"','"+explicit+"')"
+cursor.execute(sqlcommand)
+#print(sqlcommand) #debug to see SQL command
+
+#create table for playlist
+sqlcommand = "CREATE TABLE "+playlisturi+""" ("songuri"	CHAR(36) NOT NULL UNIQUE,
+       "songname"	TEXT,
+       "songrating"	NUMERIC,
+       PRIMARY KEY("songuri"));"""
+#print(sqlcommand) #debug to see SQL command
+cursor.execute(sqlcommand)
+
+
+database.commit() #actually save the database
+database.close()
