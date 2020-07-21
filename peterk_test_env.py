b7963712cb07df2987c02d14a2f6fda86d25af69
peterklem <klembczykp@wit.edu>
2020-07-21 13:49:47 -0400

    Updated crap files

--- peterk_test_env.py
+++ peterk_test_env.py
@@ -1,15 +1,17 @@
 from track_functions import Track
 from artist_functions import Artist
 from spotify_authorize import auth
+from user_functions import User
 
 
 if __name__ == '__main__':
     # Get authorization
     authorize = auth()
     sp = authorize.authorize_util()
-
-    # print(sp.artist('spotify:artist:4xRYI6VqpkE3UwrDrAZL8L'))
-    artist1 = Artist('spotify:artist:4xRYI6VqpkE3UwrDrAZL8L', sp)
-    related_artists = artist1.get_top_tracks(sp)
     
-    print(related_artists)
+   
+
+    user1 = User(sp)
+    print(user1.get_playlists(sp))
+    
+
