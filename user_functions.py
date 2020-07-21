4a7f8f0b9932dc24534e1469b45a8a1cec74812f
Arpided <66269871+Arpided@users.noreply.github.com>
2020-07-21 14:16:18 -0400

    changed final function

--- user_functions.py
+++ user_functions.py
@@ -5,16 +5,16 @@ class User():
     __uri = '' #song URI's
     __songs = [] #using this to return an array of song URI's
 
-    def __init__(self, spotify_class, user_info={}):
+    def __init__(self, uri, spotify_class, user_info={}):
         sp = spotify_class
-        #self.__uri = uri
+        self.__uri = uri
 
         if len(user_info) == 0:
-            self.__user_info = sp.me()
+            user_info = sp.user(uri)
     
     def get_followed_artists(self, spotify_class): #done
         sp = spotify_class
-        result = sp.current_user_followed_artists(limit=5)
+        result = sp.current_user_followed_artists(limit=50)
         uri_list = []
         list = result['artists']
         for i in list['items']:
@@ -37,30 +37,28 @@ class User():
             uri_list.append(i['uri'])
         return uri_list
 
-    def get_user_saved_tracks(self, spotify_class): #Done, change limits
+    def get_user_saved_tracks(self, spotify_class): #done
         sp = spotify_class
         result = sp.current_user_saved_tracks(limit=5)
         uri_list = []
-        #print(result['items'])
-        #print(result['items'][0]['track']['uri'])
-        for i in result['items']: 
+        print(result['items'])
+        for i in result['items']:
             uri_list.append(i['track']['uri'])
         return uri_list
-        #print(type(result))
-        #print(result.keys())
-        #print(list)
-        #print(result['items'])
-        #return result
 
-    def get_playlists(self, spotify_class): #having trouble with the 3rd for loop | same error as get_user_saved_tracks
+    def get_playlists(self, spotify_class): #done
         sp = spotify_class
-        result = sp.current_user_playlists(limit=5)
+        result = sp.current_user_playlists(limit=50)
         uri_playlist = []
+        name_playlist = []
         print(result.keys())
         print(type(result['items']))
+        #print(result['items'])
         print(result['items'][0].keys())
         for playlist in result['items']:
+            name_playlist.append(playlist['name'])
             uri_playlist.append(playlist['uri'])
 
-        return uri_playlist
-       
+        tup = (uri_playlist, name_playlist)
+
+        return tup
