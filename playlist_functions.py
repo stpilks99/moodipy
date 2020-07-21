6ba9823924e03437d22d08618fb673a7fbf6b1cc
galexy17 <65427264+galexy17@users.noreply.github.com>
2020-07-21 11:20:45 -0400

    Playlist functions added (incomplete)

---
+++ playlist_functions.py
@@ -0,0 +1,53 @@
+import spotipy
+from spotify_authorize import auth
+
+class Playlist:
+    #This class holds functions meant to
+    #manipulate and alter playlists.
+
+    #Current rendition: Initial Phase
+
+    #Variables
+    __uri_tracks = []
+    __uri_playlist = ''
+    __name = ''
+    moved_to_spotify = False
+    # Maybe we need more variables
+
+    def __init__(self, name, tracks):
+        #Populate local trak list
+        for track in tracks:
+            self.__uri_tracks[track] = tracks[track]
+        #Set track name, can be ''
+        self.name = name
+
+
+    def add_song(self, uri):
+        #Add track to palylist locally
+        self.__uri_tracks.append(uri)
+
+    def remove_song(self, uri):
+        #Remove song from local playlist
+        self.__uri_tracks.remove((uri))
+
+    '''def remove_playlist(self):
+
+        authorize = auth()
+        sp = authorize.authorize_util()
+
+        sp.__del__()'''
+
+
+    def set_playlist_name(self):
+        #Alter the name of the playist
+        self.__name = input("What is the name of the playlist?\n")
+
+    def move_to_spotify(self):
+        #Commit playlist to spotify
+        authorize = auth()
+        sp = authorize.authorize_util()
+        sp.user_playlist_create(sp, self.__name)
+        for track in self.__uri_tracks:
+            sp.user_playlist_add_tracks(sp,)
+            #Function requires playlist id, so this incomplete
+        #Need to incorporate the functionality of the Moved_to_Spotify flag
