40c0be9541d09ce162ed7a39b658b297f8aa7aec
peterklem <klembczykp@wit.edu>
2020-07-22 13:22:55 -0400

    Testing out different things

--- peterk_test_env.py
+++ peterk_test_env.py
@@ -2,16 +2,14 @@ from track_functions import Track
 from artist_functions import Artist
 from spotify_authorize import auth
 from user_functions import User
+from get_songs_with_criteria import get_songs_with_criteria
+import spotipy
 
 
 if __name__ == '__main__':
     # Get authorization
     authorize = auth()
     sp = authorize.authorize_util()
-    
-   
 
-    user1 = User(sp)
-    print(user1.get_playlists(sp))
-    
-
+    get_songs_with_criteria('',[],'','Juice WRLD', False, [], 0, 0, 'spotify:user:buffalobulldoggy', sp)
+        
