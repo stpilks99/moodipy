22343ad4a1038266c20d3e89bd3a9d3841b88687
peterklem <klembczykp@wit.edu>
2020-07-22 23:25:38 -0400

    tests

--- peterk_test_env.py
+++ peterk_test_env.py
@@ -5,11 +5,46 @@ from user_functions import User
 from get_songs_with_criteria import get_songs_with_criteria
 import spotipy
 
+import collections
+
 
 if __name__ == '__main__':
     # Get authorization
     authorize = auth()
     sp = authorize.authorize_util()
 
-    get_songs_with_criteria('',[],'','Juice WRLD', False, [], 0, 0, 'spotify:user:buffalobulldoggy', sp)
-        
+    #get_songs_with_criteria('sad',[],'','Juice WRLD', False, [], [], 0, 'spotify:user:buffalobulldoggy', sp)
+
+    data1 = sp.recommendations(seed_genres=['alternative'], limit=50)
+    data2 = sp.recommendations(seed_genres=['alternative'], limit=50)
+    data3 = sp.recommendations(seed_genres=['alternative'], limit=50)
+    data4 = sp.recommendations(seed_genres=['alternative'], limit=50)
+    uri1 = []
+    uri2 = []
+    uri3 = []
+    uri4 = []
+
+    for song in data1['tracks']:
+        uri1.append(song['uri'])
+    for song in data2['tracks']:
+        uri1.append(song['uri'])
+    for song in data3['tracks']:
+        uri1.append(song['uri'])
+    for song in data3['tracks']:
+        uri1.append(song['uri'])
+
+    if collections.Counter(uri1) == collections.Counter(uri2):
+        print("1 and 2.")
+    if collections.Counter(uri1) == collections.Counter(uri3):
+        print('1 and 3.')
+    if collections.Counter(uri1) == collections.Counter(uri4):
+        print('1 and 4')
+    if collections.Counter(uri2) == collections.Counter(uri3):
+        print('2 and 3')
+    if collections.Counter(uri2) == collections.Counter(uri4):
+        print('2 and 4')
+    if collections.Counter(uri3) == collections.Counter(uri4):
+        print('3 and 4')
+    
+
+    
