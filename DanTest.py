b1a4563488162f80ed41d614d55abc666405df81
Arpided <arpided@wit.edu>
2020-07-24 19:44:50 -0400

    Revert "working on functions"
    This reverts commit c9ef5a30d8d132800279ed1cbf58ee07739fd339.

--- DanTest.py
+++ DanTest.py
@@ -9,29 +9,14 @@ authorize = auth()
 sp = authorize.authorize_util()
 #print(sp)
 spot = spotipy.Spotify()
-#test = User.get_user_saved_tracks('danielj1128', sp)
-tracks = ('artist:Halsey track:Castle')
-test2 = User.testFunction('danielj1128', tracks, sp)
+#test = sp.current_user()
+test2 = User.get_username('danielj1128', sp)
 #print(test)
-print('BREAK')
-print('BREAK')
-print('BREAK')
-print('BREAK')
 print(test2)
+print('BREAK')
+print('BREAK')
+print('BREAK')
+print('BREAK')
 #artist1 = Artist('spotify:artist:4xRYI6VqpkE3UwrDrAZL8L', sp)
 #related_artists = artist1.get_top_tracks(sp)
 #print(related_artists)
-
-#def testFunction(self, uri_list, spotify_class):
-#    sp = spotify_class
-#    result = sp.tracks(uri_list)
-#    #print(result)
-#    print(result.keys())
-#    song_uri = []
-#    song_name = []
-#    #print(result['items'])
-#    for i in result['tracks']:
-#        song_uri.append(i['uri'])
-#        song_name.append(i['name'])
-#    song_list = (song_uri, song_name)
-#    return song_list
