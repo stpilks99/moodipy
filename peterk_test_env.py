14346f49ea859cdd66273e03910c7ba305b0fd9a
peterklem <klembczykp@wit.edu>
2020-07-25 09:39:30 -0400

    Testing

--- peterk_test_env.py
+++ peterk_test_env.py
@@ -11,18 +11,19 @@ import collections
 
 if __name__ == '__main__':
     # Get authorization
-    authorize = auth()
+    authorize = auth('buffalobulldoggy')
     sp = authorize.authorize_util()
+    authorize.logout()
 
     #get_songs_with_criteria('sad',[],'','Juice WRLD', False, [], [], 0, 'spotify:user:buffalobulldoggy', sp)
 
     return_uris = get_songs_with_criteria('calm', ['acoustic', 'indie'], '', '', True, [], [], 50, sp)
-    print(return_uris)
-    user1 = User(sp)
-    user_uri = user1.get_uri()
-    playlist1 = Playlist('Calming acoustic', user_uri, sp, tracks=return_uris)
-    playlist_uri = playlist1.create_spotify_playlist(sp)
-    playlist1.add_songs_sp(sp)
+    print(len(return_uris))
+    # user1 = User(sp)
+    # user_uri = user1.get_uri()
+    # playlist1 = Playlist('Calming acoustic', user_uri, sp, tracks=return_uris)
+    # playlist_uri = playlist1.create_spotify_playlist(sp)
+    # playlist1.add_songs_sp(sp)
     
     
         
