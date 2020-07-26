e21ed9a6a717cf562c4aa70377547808d0b3e38e
peterklem <klembczykp@wit.edu>
2020-07-26 14:32:07 -0400

    removed self.sp from login

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1148,8 +1148,8 @@ class login:
                 
                 #username = 'b3qviosg0fm0mqq9k0uh6uit1' # Must be changed for final version
                 authorize = auth(self.username)
-                self.sp = authorize.authorize_util() # Spotify authorized class
-                self.userClass = User(self.sp)
+                sp = authorize.authorize_util() # Spotify authorized class
+                self.userClass = User(sp)
 
                 cList = find('.cache-*', '../')
                 name = '' # Spotify username of user
