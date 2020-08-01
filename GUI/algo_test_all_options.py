e4718b2d2858ec4aa660c49b5f870e9fa0ec3780
peterklem <klembczykp@wit.edu>
2020-07-31 20:01:47 -0400

    created script for testing

--- GUI/algo_test_all_options.py
+++ GUI/algo_test_all_options.py
@@ -1,3 +1,5 @@
+# This is to test out all combinations to see which ones do not generate a list of songs
+
 from track_functions import Track
 from artist_functions import Artist
 from spotify_authorize import auth
@@ -8,15 +10,45 @@ import spotipy
 
 if __name__ == '__main__':
 
-    # This is to test out all combinations to see which ones do not generate a list of songs
-
+    # Authorize Spotify connection
+    authorize = auth('buffalobulldoggy')
+    sp = authorize.authorize_util()
+    
+    # All genres in project so far
     genres1 = ['Acoustic', 'Afrobeat', 'Alternative', 'Ambient', 'Brazil', 'Classical']
     genres2 = ['Club', 'Country', 'Disco', 'Dubstep', 'EDM', 'Funk']
     genres3 = ['Gospel', 'Hard-rock', 'Heavy-metal', 'Hip-hop', 'Holidays', 'Indie']
     genres4 = ['Jazz', 'K-pop', 'Latin', 'Metal', 'Pop', 'Punk']
     genres5 = ['Reggae', 'R-n-b', 'Rock', 'Soul']
 
-    all_genres = genres1 + genres2 + genres3 + genres4 + genres5
-    print(all_genres)
+    genres = genres1 + genres2 + genres3 + genres4 + genres5 
+    #print(genres)
 
-    moods = ['Happy', 'Sad', ]
+
+    moods = ['Happy', 'Sad', 'Calm', 'Motivated', 'Frantic', 'Party', 'Gaming']
+
+    song_lim = 30 # Number of songs returned from Spotify
+
+    successes = [] 
+    fails = []
+    # Loop through each combination and try
+    for i in range(len(moods)):
+        for j in range(len(genres)):
+            # The genres must be in the format of a list
+            genre_as_list = []
+            genre_as_list.append(genres[j])
+            # try:
+            uris = get_songs_with_criteria(moods[i], genre_as_list, '', [], [], song_lim, sp)
+            # except:
+            #     fails.append((moods[i], genres[j]))
+            #     continue
+
+            if len(uris) != song_lim: # Number of desired songs has not been reached
+                fails.append((moods[i], genres[j]))
+            else:
+                successes.append((moods[i], genres[j]))
+
+    print('Failed combinations:')       
+    for combo in fails:
+        print('Mood: ', combo[0], ', genre: ', combo[1])
+            
